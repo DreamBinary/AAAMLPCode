983ea1d6d44a552891f8e3da0687c81032df3be0
cxq <1647715359@qq.com>
2024-02-26 21:38:13 +0800

    update

--- tmp.py
+++ tmp.py
@@ -4,3 +4,13 @@
 # @Author : fiv
 
 
+import pandas as pd
+from sklearn import datasets
+
+data = datasets.fetch_openml(
+    "mnist_784",
+    version=1,
+    return_X_y=True
+)
+pixel_values, targets = data
+pixel_values.reshape()
