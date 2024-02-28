2288cd1b22b0f85b4358c297def0975a37c66744
cxq <1647715359@qq.com>
2024-02-28 16:02:19 +0800

    update

--- tmp.py
+++ tmp.py
@@ -4,13 +4,36 @@
 # @Author : fiv
 
 
-import pandas as pd
-from sklearn import datasets
+class AllOne:
 
-data = datasets.fetch_openml(
-    "mnist_784",
-    version=1,
-    return_X_y=True
-)
-pixel_values, targets = data
-pixel_values.reshape()
+    def __init__(self):
+        self.data = {}
+
+    def inc(self, key: str) -> None:
+        if key in self.data:
+            self.data[key] += 1
+        else:
+            self.data[key] = 1
+
+    def dec(self, key: str) -> None:
+        if key in self.data:
+            self.data[key] -= 1
+            if self.data[key] == 0:
+                del self.data[key]
+
+    def getMaxKey(self) -> str:
+        if len(self.data) == 0:
+            return ""
+        return max(self.data, key=self.data.get)
+
+    def getMinKey(self) -> str:
+        if len(self.data) == 0:
+            return ""
+        return min(self.data, key=self.data.get)
+
+    # Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
